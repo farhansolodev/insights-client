e8da15703ea05ecaff950ecff6c5f3045c959e0c
Farhan Alvi <52133344+farhansolodev@users.noreply.github.com>
2022-03-19 21:03:54 +0400

    Update cli.js

--- bin/cli.js
+++ bin/cli.js
@@ -4,7 +4,7 @@ const { execSync } = require('child_process')
 
 const runCmd = cmd => {
    try {
-       execSync(`${cmd}`, { stdio: 'inherit', cwd: __dirname })
+       execSync(`${cmd}`, { stdio: 'inherit', cwd: `${__dirname.replace(/bin/g,"")}` })
    } catch (e) {
        console.error(`Failed to execute ${cmd}`, e)
        return false
@@ -12,22 +12,6 @@ const runCmd = cmd => {
    return true
 }
 
-const cmd = `cd ${__dirname.replace(/bin/g,"")} && npm start`
-// const cmd = `cd c:/ && dir`
+const cmd = `npm start`
 
 runCmd(cmd)
-
-// const repo = "insights-client"
-// const gitCheckoutCmd =  `\ngit clone --depth 1 https://github.com/farhansolodev/${repo}\n`
-// const installDepsCmd = `cd ${repo} && npm install`
-
-// console.log(`\nCloning the repository with name ${repo}`)
-// const checkedOut = runCmd(gitCheckoutCmd)
-// if (!checkedOut) process.exit(-1)
-
-// console.log(`\nInstalling dependencies for ${repo}\n`)
-// const installedDeps = runCmd(installDepsCmd)
-// if (!installedDeps) process.exit(-1)
-
-// console.log('\nSetup successful! Starting application...')
-// console.log(`\ncd ${repo} && npm start\n`)
