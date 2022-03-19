4337accbc98300f7d3b9e761035dda2102db6c09
Farhan Alvi <52133344+farhansolodev@users.noreply.github.com>
2022-03-19 04:53:19 +0400

    back to testing
    testing if i can cd to npm_cache and simply run the project from there to avoid redownloading deps since there are so many deps already

--- bin/cli.js
+++ bin/cli.js
@@ -12,17 +12,19 @@ const runCmd = cmd => {
    return true
 }
 
-const repo = "insights-client"
-const gitCheckoutCmd =  `\ngit clone --depth 1 https://github.com/farhansolodev/${repo}\n`
-const installDepsCmd = `cd ${repo} && npm install`
+runCmd("echo %AppData%/npm-cache")
 
-console.log(`\nCloning the repository with name ${repo}`)
-const checkedOut = runCmd(gitCheckoutCmd)
-if (!checkedOut) process.exit(-1)
+// const repo = "insights-client"
+// const gitCheckoutCmd =  `\ngit clone --depth 1 https://github.com/farhansolodev/${repo}\n`
+// const installDepsCmd = `cd ${repo} && npm install`
 
-console.log(`\nInstalling dependencies for ${repo}\n`)
-const installedDeps = runCmd(installDepsCmd)
-if (!installedDeps) process.exit(-1)
+// console.log(`\nCloning the repository with name ${repo}`)
+// const checkedOut = runCmd(gitCheckoutCmd)
+// if (!checkedOut) process.exit(-1)
 
-console.log('\nSetup successful! Starting application...')
-console.log(`\ncd ${repo} && npm start\n`)
+// console.log(`\nInstalling dependencies for ${repo}\n`)
+// const installedDeps = runCmd(installDepsCmd)
+// if (!installedDeps) process.exit(-1)
+
+// console.log('\nSetup successful! Starting application...')
+// console.log(`\ncd ${repo} && npm start\n`)
