6aeaf6d14ae1fdcb9b7043d63c1c997d4c58b03a
farhansolodev <farhanalvi005@gmail.com>
2022-03-19 04:44:27 +0400

    cli complete

--- bin/cli.js
+++ bin/cli.js
@@ -12,19 +12,17 @@ const runCmd = cmd => {
    return true
 }
 
-runCmd("mkdir test")
+const repo = "insights-client"
+const gitCheckoutCmd =  `\ngit clone --depth 1 https://github.com/farhansolodev/${repo}\n`
+const installDepsCmd = `cd ${repo} && npm install`
 
-// const repo = "insights-client"
-// const gitCheckoutCmd =  `\ngit clone --depth 1 https://github.com/farhansolodev/${repo}\n`
-// const installDepsCmd = `cd ${repo} && npm install`
-//
-// console.log(`\nCloning the repository with name ${repo}`)
-// const checkedOut = runCmd(gitCheckoutCmd)
-// if (!checkedOut) process.exit(-1)
-//
-// console.log(`\nInstalling dependencies for ${repo}\n`)
-// const installedDeps = runCmd(installDepsCmd)
-// if (!installedDeps) process.exit(-1)
-//
-// console.log('\nCongratulations! You are ready. Run the following commands to start:')
-// console.log(`\ncd ${repo} && npm start\n`)
+console.log(`\nCloning the repository with name ${repo}`)
+const checkedOut = runCmd(gitCheckoutCmd)
+if (!checkedOut) process.exit(-1)
+
+console.log(`\nInstalling dependencies for ${repo}\n`)
+const installedDeps = runCmd(installDepsCmd)
+if (!installedDeps) process.exit(-1)
+
+console.log('\nSetup successful! Starting application...')
+console.log(`\ncd ${repo} && npm start\n`)
