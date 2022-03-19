2de463efe1ff3297823b43b26e962758d116818d
farhansolodev <farhanalvi005@gmail.com>
2022-03-19 04:30:37 +0400

    testing cd

--- bin/cli.js
+++ bin/cli.js
@@ -12,19 +12,19 @@ const runCmd = cmd => {
    return true
 }
 
-runCmd("dir")
+runCmd("cd src")
 
-//const repo = process.argv[2]
-//const gitCheckoutCmd =  `git clone --depth 1 https://github.com/farhansolodev/insights-client ${repo}`
-//const installDepsCmd = `cd ${repo} && npm install`
+// const repo = "insights-client"
+// const gitCheckoutCmd =  `\ngit clone --depth 1 https://github.com/farhansolodev/${repo}\n`
+// const installDepsCmd = `cd ${repo} && npm install`
 //
-//console.log(`Cloning the repository with name ${repo}`)
-//const checkedOut = runCmd(gitCheckoutCmd)
-//if (!checkedOut) process.exit(-1)
+// console.log(`\nCloning the repository with name ${repo}`)
+// const checkedOut = runCmd(gitCheckoutCmd)
+// if (!checkedOut) process.exit(-1)
 //
-//console.log(`Installing dependencies for ${repo}`)
-//const installedDeps = runCmd(installDepsCmd)
-//if (!installedDeps) process.exit(-1)
+// console.log(`\nInstalling dependencies for ${repo}\n`)
+// const installedDeps = runCmd(installDepsCmd)
+// if (!installedDeps) process.exit(-1)
 //
-//console.log('Congratulations! You are ready. Run the following commands to start:')
-//console.log(`\ncd ${repo} && npm start\n`)
+// console.log('\nCongratulations! You are ready. Run the following commands to start:')
+// console.log(`\ncd ${repo} && npm start\n`)
