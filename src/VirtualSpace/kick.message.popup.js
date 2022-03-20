59ee68c61c67d0b6cf2f250c9e49b95c95257664
SusanDz <sd2009@hw.ac.uk>
2022-03-20 14:17:44 +0400

    create a popup to notify user being kicked

---
+++ src/VirtualSpace/kick.message.popup.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+// import { db } from "../Firebase/firebase";
+// import { getDoc, doc } from "firebase/firestore"; 
+import { io } from "socket.io-client"
+import styles from '../styles/form.module.css';
+
+const KickMessagePopup = ({ username, onSubmit, onCancel }) => {
+    const userId = null
+    const s = io("https://insights--server.herokuapp.com")
+    // useEffect(() => {
+    //     s.on("user-kicked", ({ uId, uname }) => {
+    //         userId = uId
+    //     })
+
+    //     return () => {
+	// 		if (!userId) return
+	// 		console.log("Disconnecting from Virtual Space with socket: ", s)
+	// 		s.disconnect()
+	// 	}
+    // }, [userId])
+
+    onSubmit(e);
+    
+    return (
+        <>
+            <div className={styles["container"]}>
+                <h2>Message</h2>
+                <div className={styles["form_group"]}>
+                    <h4>You are being kicked from this Virtual Space</h4>
+                </div>
+            </div>
+        </>
+     );
+}
+
+export default KickMessagePopup;
