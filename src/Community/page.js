db7d52e8e6ac4f6c75565eb8e1910cb264833331
SusanDz <sd2009@hw.ac.uk>
2022-03-28 18:36:28 +0400

    Done with rendering of Community image, community collabs and about Container of community(along with all the database queries)

--- src/Community/page.js
+++ src/Community/page.js
@@ -1,65 +1,68 @@
-import React, { useState, useEffect } from "react";
-import { useLocation } from "react-router-dom";
-import { useHistory, useParams } from "react-router-dom";
+import React, { useState, useEffect } from "react"
+import { useHistory, useParams } from "react-router-dom"
 import AppBar from "../AppBar/bar"
 import styles from "./community.module.css"
-import { AppBarButtons } from "./appbar.buttons";
-import Posts from "./community.posts";
-import { collection, doc, getDoc, getDocs, query, where } from "firebase/firestore";
-import { db } from "../firebase";
-import { async } from "@firebase/util";
-import { useUser } from "../context/user";
+import { AppBarButtons } from "./appbar.buttons"
+import Posts from "./community.posts"
+import { collection, doc, getDoc, getDocs, query, where } from "firebase/firestore"
+import { db } from "../firebase"
+import { useUser } from "../context/user"
 
 
 const Commmunity = ({name}) => {
     
     const {id: comName } = useParams();
-    //FIREBASE STUFF for comData
     const [comId, setComId] = useState();
     const [comData, setComData] = useState([]);
-    const [collabData, setCollabData] = useState();
+    const [collabData, setCollabData] = useState([]);
     const { userData } = useUser();
     const [appBarStatus, setAppBarStatus] = useState('');
 
     //this should be run when someone opens the community page to check if user is a member or not to initialise the isMember field
-    async function checkMember() {
-        if(comData.members.includes(userData.id)) {
+    function setMembership() {
+        if (comData.members.includes(userData.id)) {
             setIsMember(true)
         } else {
             setIsMember(false)
         }
     }
-    const [isMember, setIsMember] = useState(false); 
 
-    const communityName = "Soulsborne Community" //set this as comData.name
+    const [isMember, setIsMember] = useState(false); 
     
     const history = useHistory();
 
     //what i couldnt do :(
     useEffect( () => {
+        // if(comData) return
         const q = query(collection(db, "communities"), where("name", "==", comName))
         getDocs(q).then((snapshot) => {
             snapshot.forEach(async (doc) => {
                 setComId(doc.id)
                 setComData(doc.data())
                 // setComData(prev => [...prev,doc.data()])
-                console.log(doc.data())
+                // console.log(doc.data())
             });
         })
+    }, [comName])
+
+    useEffect(() => {
+        // console.log(comData)
+        if(collabData.length !== 0) return
+        if(!comData?.publishedCollabs) return
         comData.publishedCollabs.forEach(collabId => {
             getDoc(doc(db, "collabs", collabId)).then(snap => {
 				const collab = snap.data()
 				setCollabData(prev => {
-					return {
+					return [
 						...prev,
-						[collabId]: collab
-					}
+						{ id: collabId, ...collab }
+                    ]
 				})
 			})
         })
-    }, [comName])
+    }, [comData])
     
-    console.log(comData);
+    // console.log(comData);
 
     function join() {
         //comData.members append user.id
@@ -71,28 +74,29 @@ const Commmunity = ({name}) => {
         setIsMember(false);
     }
 
-    // function onStatusChange(e) {
-	// 	e.preventDefault()
-	// 	const val = e.target.id
-	// 	setAppBarStatus(val)
+    function onStatusChange(e) {
+		e.preventDefault()
+		const val = e.target.id
+		setAppBarStatus(val)
 
-    //     appBarStatus==="join-community" ? join() : leave();
-	// }
+        appBarStatus==="join-community" ? join() : leave();
+	}
 
     return (
         <>
-            <AppBar title={communityName} buttons={isMember ? [AppBarButtons.leave] : [AppBarButtons.join]}  />
+            <AppBar title={comName} buttons={isMember ? [AppBarButtons.leave] : [AppBarButtons.join]} onClickHandler={onStatusChange} />
             <div className={styles["community-container"]}>
-                <div className={styles.image}>
-                </div>
+                <div style={{backgroundImage: `url(${comData?.image})`}} className={styles.image}/>
+                {/* <div className={styles.body}> */}
                 <div className={styles["posts-container"]}>
+                    {console.log("Content",collabData)}
                     {
-                        comData?.publishedCollabs.map(({ name, content }, index) => {
-                            return <Posts key={index} name={name} content={content}/>
+                        collabData && collabData.map(({ id, name, virtualSpaceId, content, likes, usersLiked }, index) => {
+                            // return <div key={index} >{`${name} ${JSON.stringify(content)}`}</div>
+                            return <Posts virtualSpaceId={virtualSpaceId} isMember key={index} id={id} name={name} content={JSON.stringify(content)} likeVal={likes} usersLiked={usersLiked} />
                         })
                     }
                     {/* map the community published collabs and pass each collab id for posts*/}
-                    {/* <Posts /*collabs={collab.data()*//> */}
                 </div>
                 <div className={styles["about-container"]}>
                     <div className={styles["about-box"]}>
@@ -100,13 +104,14 @@ const Commmunity = ({name}) => {
                             About community
                         </div>
                         <div className={styles["description"]}>
-                            {/*comData.description */}Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+                            {comData.description}
                         </div>
                         <div className={styles["members"]}>
-                            Members: {/*comData.members.length() + 1*/}
+                            Members: {comData.members?.length}
                         </div>
                     </div>
-                </div>    
+                </div>
+                {/* </div>  */}
             </div>
         </>
     )
